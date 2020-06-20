2d27dfb273991fd639f8e8a0e940cb00167dc6f2
Ben Hogan <j.benhogan@gmail.com>
2020-06-20 14:33:48 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,10 +3,12 @@
 
 def key_for_min_value(name_hash)
   least_value = 50000000000
+  least_key = ""
   name_hash.each do |key, value|
     if value < least_value
       least_value = value
+      least_key = key
     end
   end
-  name_hash[least_value]
+  least_key
 end
