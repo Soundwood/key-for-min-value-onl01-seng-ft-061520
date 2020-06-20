abb511a02f007c8d31dacb3159068398400a4c33
Ben Hogan <j.benhogan@gmail.com>
2020-06-20 14:35:47 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   if name_hash.empty?
     return nil
   else
-    least_value = 50000000000
+    least_value = 9999999999999
     least_key = ""
     name_hash.each do |key, value|
       if value < least_value
